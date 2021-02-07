adca044f0af3225f4415ca2491a07afcb560d4ac
Michael <michael52423@gmail.com>
2021-02-07 21:50:27 +1100

    Revised initial commit

---
+++ routes/web.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const chatbotController = require("../controllers/chatbotController");
+
+const initWebRoutes = (app) => {
+  router.get("/", chatbotController.test);
+  router.get("/webhook", chatbotController.getWebhook);
+  router.post("/webhook", chatbotController.postWebhook);
+
+  return app.use("/", router);
+};
+
+module.exports = initWebRoutes;
